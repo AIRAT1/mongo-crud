b374f3c4640f4f9f9e7b4c94d935b305f35f1d32
Ayrat <ayrat1@mail.ru>
2020-11-23 00:49:45 +0300

    init project

---
+++ src/main/java/de/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package de.repository;
+
+import de.domain.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student, Integer> {
+}
---
+++ src/test/java/de/CRUD/with/mongo/db/and/vaadin/CrudWithMongoDbAndVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package de.CRUD.with.mongo.db.and.vaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithMongoDbAndVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
